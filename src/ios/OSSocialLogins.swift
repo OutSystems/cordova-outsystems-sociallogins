809a38e09d50312db897001c1c30d9b1b8ff1d7c
Carlos Corrêa <carlscorrea@gmail.com>
2021-11-23 16:34:42 +0000

    feat: added swift support, created the first classes and added core framework

---
+++ src/ios/OSSocialLogins.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+@objc(OSSocialLogins)
+class OSSocialLogins: CordovaImplementation {
+    var plugin: SocialLoginsPlugin?
+    var callbackId:String=""
+    
+    override func pluginInitialize() {
+        plugin = SocialLoginsPlugin()
+    }
+    
+    @objc(doLogin:)
+    func doLogin(command: CDVInvokedUrlCommand) {
+        callbackId = command.callbackId
+        
+        plugin?.doLogin()
+        self.sendResult(result: "", error:err , callBackID: self.callbackId)
+    }
+       
+}
---
+++ src/ios/SocialLoginsPlugin.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+class SocialLoginsPlugin {
+
+    func doLogin() {
+        print("called doLogin!")
+    }
+
+}
