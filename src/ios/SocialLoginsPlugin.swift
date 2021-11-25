2d6a891220cfcabfe769f437e4c4d7d6c99e8320
Carlos Corrêa <carlscorrea@gmail.com>
2021-11-25 10:24:49 +0000

    feat: calling authorization view controller

--- src/ios/SocialLoginsPlugin.swift
+++ src/ios/SocialLoginsPlugin.swift
@@ -1,9 +1,51 @@
 import Foundation
+import AuthenticationServices
 
-class SocialLoginsPlugin {
-
+class SocialLoginsPlugin: NSObject, ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
+    
+    var rootViewController:UIViewController?
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.rootViewController!.view as! ASPresentationAnchor
+    }
+    
+    override init() {
+        super.init()
+//        performExistingAccountSetupFlows()
+    }
+    
     func doLogin() {
         print("called doLogin!")
+        handleAuthorizationAppleIDButtonPress()
+    }
+    
+    func performExistingAccountSetupFlows() {
+        let requests = [ASAuthorizationAppleIDProvider().createRequest(),
+                        ASAuthorizationPasswordProvider().createRequest()]
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: requests)
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    func handleAuthorizationAppleIDButtonPress() {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
     }
 
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as?  ASAuthorizationAppleIDCredential {
+        let userIdentifier = appleIDCredential.user
+        let fullName = appleIDCredential.fullName
+        let email = appleIDCredential.email
+        print("User id is \(userIdentifier) \n Full Name is \(String(describing: fullName)) \n Email id is \(String(describing: email))") }
+    }
+    
 }
