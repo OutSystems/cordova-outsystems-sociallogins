5044eeb003de75a67f73abfb424dad43bb4f75c2
Carlos Corrêa <carlscorrea@gmail.com>
2021-11-26 14:25:01 +0000

    feat: apple login implemented

--- src/ios/OSSocialLogins.swift
+++ src/ios/OSSocialLogins.swift
@@ -2,19 +2,46 @@ import Foundation
 
 @objc(OSSocialLogins)
 class OSSocialLogins: CordovaImplementation {
+    
     var plugin: SocialLoginsPlugin?
     var callbackId:String=""
     
     override func pluginInitialize() {
         plugin = SocialLoginsPlugin()
+        plugin?.rootViewController = self.viewController
+        plugin?.delegate = self
     }
     
     @objc(doLogin:)
     func doLogin(command: CDVInvokedUrlCommand) {
         callbackId = command.callbackId
         
-        plugin?.doLogin()
-        self.sendResult(result: "", error:nil , callBackID: self.callbackId)
+        do {
+            try plugin?.doLogin(callbackID: self.callbackId)
+        } catch {
+            self.sendResult(result: "", error:nil , callBackID: self.callbackId)
+        }
+    }
+    
+    @objc(isLoggedIn:)
+    func isLoggedIn(command: CDVInvokedUrlCommand) {
+        callbackId = command.callbackId
+        
+        do {
+            try plugin?.getCredentialState(userID: "")
+        } catch {
+            self.sendResult(result: "", error:nil , callBackID: self.callbackId)
+        }
     }
        
+}
+
+extension OSSocialLogins: SocialLoginsProtocol {
+    func callBackUserInfoResponse(result: UserInfoResponse?, error: NSError?, callBackID: String) {
+        let finalResult = result?.encode(object:result)
+        self.sendResult(result: finalResult, error:nil , callBackID: self.callbackId)
+    }
+    func callBackCredentialState(result: String?, error: NSError?, callBackID: String) {
+        self.sendResult(result: result, error:nil , callBackID: self.callbackId)
+    }
 }
