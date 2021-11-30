a167e6b92aa3f0caac4186a2ecc0db091d13f70d
alexgerardojacinto <alexandre.jacinto@outsystems.com>
2021-11-30 12:03:52 +0000

    feat: add login and logout actions to JS layer
    References: https://outsystemsrd.atlassian.net/browse/RMET-1152

--- www/OSSocialLogins.js
+++ www/OSSocialLogins.js
@@ -3,3 +3,11 @@ var exec = require('cordova/exec');
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'OSSocialLogins', 'coolMethod', [arg0]);
 };
+
+exports.login = function (arg0, success, error) {
+    exec(success, error, 'OSSocialLogins', 'login', [arg0]);
+};
+
+exports.logout = function (arg0, success, error) {
+    exec(success, error, 'OSSocialLogins', 'logout', [arg0]);
+};
