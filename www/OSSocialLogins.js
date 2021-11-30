356fbd5d3dcda2087c52fa57d9bb6f40491a8143
Carlos Corrêa <carlscorrea@gmail.com>
2021-11-30 19:24:05 +0000

    ios target updated to 13

--- www/OSSocialLogins.js
+++ www/OSSocialLogins.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.doLogin = function (success, error) {
     exec(success, error, 'OSSocialLogins', 'doLogin');
 };
+
+exports.isLoggedIn = function (success, error, arg0) {
+    exec(success, error, 'OSSocialLogins', 'isLoggedIn', [arg0]);
+};
