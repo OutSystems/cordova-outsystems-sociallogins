b12e399f92696ab610ebf02c134bf3ea119278fe
alexgerardojacinto <alexandre.jacinto@outsystems.com>
2021-12-02 19:08:04 +0000

    fix: fixing action name

--- www/OSSocialLogins.js
+++ www/OSSocialLogins.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec');
 
 exports.doLogin = function (success, error, provider) {
-    exec(success, error, 'OSSocialLogins', 'login', [provider]);
+    exec(success, error, 'OSSocialLogins', 'doLogin', [provider]);
 };
 
 exports.logout = function (arg0, success, error) {
