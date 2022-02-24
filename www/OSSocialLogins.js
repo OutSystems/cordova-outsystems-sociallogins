b1e9b43199d0a5f1e5b57ea56fc086f0de399cab
Carlos Corrêa <carlscorrea@gmail.com>
2022-02-24 12:52:17 +0000

    feat: updating javascript

--- www/OSSocialLogins.js
+++ www/OSSocialLogins.js
@@ -4,6 +4,10 @@ exports.login = function (success, error, provider) {
     exec(success, error, 'OSSocialLogins', 'login', [provider]);
 };
 
+exports.loginApple = function (success, error, state, clientId, redirectUrl) {
+    exec(success, error, 'OSSocialLogins', 'login', [state, clientId, redirectUrl]);
+};
+
 exports.logout = function (arg0, success, error) {
     exec(success, error, 'OSSocialLogins', 'logout', [arg0]);
 };
