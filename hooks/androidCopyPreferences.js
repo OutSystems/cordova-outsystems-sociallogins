47bece876c520efe9fb21b901a7d5c5df6eefe9a
alexgerardojacinto <alexandre.jacinto@outsystems.com>
2022-02-25 13:41:55 +0000

    refactor: delete logs for troubleshooting

--- hooks/androidCopyPreferences.js
+++ hooks/androidCopyPreferences.js
@@ -10,34 +10,17 @@ module.exports = function (context) {
     var configParser = new ConfigParser(configXML);
     var default_hostname = configParser.getGlobalPreference("DefaultHostname");
 
-    console.log("default_hostname: " + default_hostname);
-
-    
     //go inside the AndroidManifest and change value for DEFAULT_HOSTNAME
     var manifestPath = path.join(projectRoot, 'platforms/android/app/src/main/AndroidManifest.xml');
     var manifestFile = fs.readFileSync(manifestPath).toString();
     var etreeManifest = et.parse(manifestFile);
 
-    console.log("ElementTree fez o parse do manifest");
-
-    var dataTags1 = etreeManifest.findall('./application/activity/intent-filter/data[@android:scheme]');
-    console.log("Number of data tags: " + dataTags1.length);
-
-    var appleActivityTags = etreeManifest.findall('./application/activity[@android:name="com.outsystems.plugins.sociallogins.AppleSignInActivity"]');
-    console.log("Number of apple activity tags: " + appleActivityTags.length);
-
     var dataTags = etreeManifest.findall('./application/activity/intent-filter/data[@android:host="DEFAULT_HOSTNAME"]');
-    console.log("dataTags.length: " + dataTags.length);
-
     for (var i = 0; i < dataTags.length; i++) {
         var data = dataTags[i];
         data.set("android:host", default_hostname);
-        console.log("entrou no for");
     }
 
     var resultXmlManifest = etreeManifest.write();
     fs.writeFileSync(manifestPath, resultXmlManifest);
-
-    console.log("acabou, fez o write");
-    
 };
