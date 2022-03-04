1199025c692a1def624af014c08a0b5923fdcd2e
OS-ricardomoreirasilva <ricardo.moreira.silva@outsystems.com>
2022-03-04 10:52:39 +0000

    AppDelegate Method Swizzle
    Apply method swizzling so that both application:didFinishLaunchingWithOptions: methods (the Cordova managed one and the SocialLogins extension) are properly called.

--- src/ios/AppDelegate+OSSocialLogins.m
+++ src/ios/AppDelegate+OSSocialLogins.m
@@ -1,10 +1,18 @@
 #import "AppDelegate+OSSocialLogins.h"
 
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <objc/runtime.h>
 
 @implementation AppDelegate (OSSocialLogins)
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
++ (void)load {
+    Method original = class_getInstanceMethod(self, @selector(application:didFinishLaunchingWithOptions:));
+    Method swizzled = class_getInstanceMethod(self, @selector(application:swizzledDidFinishLaunchingWithOptions:));
+    method_exchangeImplementations(original, swizzled);
+}
+
+- (BOOL)application:(UIApplication *)application swizzledDidFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [self application:application swizzledDidFinishLaunchingWithOptions:launchOptions];
     
     [FBSDKApplicationDelegate.sharedInstance application:application didFinishLaunchingWithOptions:launchOptions];
     
