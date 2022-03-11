5b8d85cf2fb5c60e32f5ad7e0049a0d4efc183c4
alexgerardojacinto <alexandre.jacinto@outsystems.com>
2022-03-11 11:40:32 +0000

    refactor: remove unused methods

--- src/android/com/outsystems/plugins/sociallogins/SocialLoginsGoogleController.kt
+++ src/android/com/outsystems/plugins/sociallogins/SocialLoginsGoogleController.kt
@@ -22,10 +22,6 @@ class SocialLoginsGoogleController {
     private val GOOGLE_SIGN_IN: Int = 2
 
     fun doLogin(activity: Activity){
-
-        //maybe here we should call silentSignIn or check if the user is already signed in first to deal with the case where the user has already signed in
-        //TODO
-
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         // Configure sign-in to request the user's ID, email address, and basic
@@ -43,60 +39,7 @@ class SocialLoginsGoogleController {
         signIn(mGoogleSignInClient, activity)
     }
 
-    //Maybe we could keep this to avoid asking the user to login if he/she is already logged in and we can just return the data back
-    fun isUserLoggedIn(context: Context) : Pair<Boolean, GoogleSignInAccount?>{
-        val account = GoogleSignIn.getLastSignedInAccount(context)
-        if(account != null){
-            return Pair(true, account)
-        }
-        return Pair(false, null)
-    }
-
-    /*
-    fun getLoginData(onSuccess : (UserInfo) -> Unit, onError : (SocialLoginError) -> Unit) {
-
-        val isLoggedIn = isUserLoggedIn()
-        if(isLoggedIn.first){
-            onSuccess(UserInfo(
-                isLoggedIn.second!!.id,
-                isLoggedIn.second!!.email,
-                isLoggedIn.second!!.displayName,
-                isLoggedIn.second!!.photoUrl.toString(),
-                "" // DO WE NEED THIS FOR GOOGLE? DONT THINK SO
-            )
-            )
-        }
-        else{
-            onError(SocialLoginError.NO_USER_LOGGED_IN_ERROR)
-        }
-    }
-     */
-
     private fun signIn(signInClient: GoogleSignInClient, activity: Activity) {
-
-        //to discuss, do we want to do the login silently when possible?
-        //or do we want to present the login dialog to the user every time?
-        //for now, I'll leave it commented
-        //if we want to do silentSignIn, then we have to send the result of the login here
-        //and not through the OnActivityResult
-        /*
-        signInClient.silentSignIn()
-            .addOnCompleteListener(
-                activity,
-                OnCompleteListener<GoogleSignInAccount> {
-                        task -> handleSignInResult(task,
-                    {
-                        it.email
-                        it.firstName
-                    },
-                    {
-                        it.code
-                        it.message
-                    })
-                }
-            )
-         */
-
         val signInIntent: Intent = signInClient.signInIntent
         startActivityForResult(activity, signInIntent, GOOGLE_SIGN_IN, null)
     }
