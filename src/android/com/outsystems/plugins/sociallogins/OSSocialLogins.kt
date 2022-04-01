a43439312766a080839950519342a397a02cdd33
alexgerardojacinto <alexandre.jacinto@outsystems.com>
2022-04-01 18:47:50 +0100

    fix: fixing imports

--- src/android/com/outsystems/plugins/sociallogins/OSSocialLogins.kt
+++ src/android/com/outsystems/plugins/sociallogins/OSSocialLogins.kt
@@ -2,8 +2,13 @@ package com.outsystems.plugins.sociallogins
 
 import android.content.Intent
 import com.google.gson.Gson
+import com.outsystems.plugins.sociallogins.apple.AppleHelper
+import com.outsystems.plugins.sociallogins.apple.SocialLoginsAppleController
 import com.outsystems.plugins.sociallogins.facebook.FacebookHelper
 import com.outsystems.plugins.sociallogins.facebook.SocialLoginsFacebookController
+import com.outsystems.plugins.sociallogins.google.GoogleHelper
+import com.outsystems.plugins.sociallogins.google.SocialLoginsGoogleController
+import com.outsystems.plugins.sociallogins.linkedin.SocialLoginsLinkedinController
 import org.apache.cordova.CallbackContext
 import org.apache.cordova.CordovaInterface
 import org.apache.cordova.CordovaWebView
