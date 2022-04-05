ba94874229a77d984fdac9387cf6223ceb281360
Nelson Lopes Silva <5671236+nflsilva@users.noreply.github.com>
2022-04-05 12:08:45 +0100

    refactor: added OSSocialLoginsLib and OS-CoreLib dependencies

--- src/android/com/outsystems/plugins/sociallogins/LinkedInConstants.kt
+++
@@ -1,9 +0,0 @@
-package com.outsystems.plugins.sociallogins.linkedin
-
-object LinkedInConstants {
-    const val SCOPE = "r_liteprofile%20r_emailaddress"
-    const val AUTH_URL = "https://www.linkedin.com/oauth/v2/authorization"
-    const val RESPONSE_TYPE = "code"
-    const val LINKEDIN_REQUEST_CODE = 4;
-    const val LINKEDIN_RESULT_CODE = 4
-}
