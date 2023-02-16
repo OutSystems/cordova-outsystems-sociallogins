5b44e4d7de9ed89cd5d3c797d706437c8e207a46
Marta Carlos <101343976+OS-martacarlos@users.noreply.github.com>
2023-02-16 14:37:15 +0000

    RMET-2252 :: Remove JSON file dependency , add Endpoint (#38)
    * feat: add endpoint logic to android hook
    
    * fix: add config parser import
    
    * removing fetch + add axios
    
    * minor typo fix
    
    * feat: add get endpoint to iOS
    
    * remove static dependency
    
    * adding more error handling
    
    * chore: refactor
    
    * refactor: rename preference to be more accurate
    
    * reactor: change preference according to Dev Exp suggestion
    
    * fix: change error accordingly

--- hooks/androidCopyPreferences.js
+++ hooks/androidCopyPreferences.js
@@ -1,76 +1,66 @@
 const et = require('elementtree');
 const path = require('path');
 const fs = require('fs');
+const { ConfigParser } = require('cordova-common');
 
-const ProvidersEnum = Object.freeze({"apple":"1", "facebook":"2", "google":"3", "linkedIn":"4"})
-const ApplicationTypeEnum = Object.freeze({"web":"1", "ios":"2", "android":"3"})
+const {getJsonFile, ProvidersEnum, ApplicationTypeEnum} = require('./utils');
 
-module.exports = function (context) {
+module.exports = async function (context) {
+    let projectRoot = context.opts.cordova.project ? context.opts.cordova.project.root : context.opts.projectRoot;
+    let configXML = path.join(projectRoot, 'config.xml');
+    let configParser = new ConfigParser(configXML);
+    let configuratorURL = configParser.getGlobalPreference("SOCIAL_CONF_API_ENDPOINT");
+    
+    if(configuratorURL.length == 0)
+        throw new Error("Missing preference: SOCIAL_CONF_API_ENDPOINT. Please make sure this preference is configured");
+    
+    let appName = configParser.name();
+    
+    let jsonConfig = await getJsonFile(configuratorURL, appName);
+    copyFacebookPreferences(jsonConfig, projectRoot);
+    copyLinkedInPreferences(jsonConfig, projectRoot);
+};
 
-    const configFileName = 'www/json-config/SocialLoginsConfigurations.json';
-    var linkedin_deeplink_url = "";
-    var linkedin_deeplink_host = "";
-    var linkedin_deeplink_path = "";
-    var projectRoot = context.opts.cordova.project ? context.opts.cordova.project.root : context.opts.projectRoot;
+function copyLinkedInPreferences(jsonConfig, projectRoot) {
+    let linkedin_deeplink_url = "";
+    let linkedin_deeplink_host = "";
+    let linkedin_deeplink_path = "";
 
-    copyFacebookPreferences(projectRoot);
-
-    //read json config file       www/jsonConfig/sociallogins_configurations.json
-    var jsonConfig = "";
     try {
-        jsonConfig = path.join(projectRoot, configFileName);
-        var jsonConfigFile = fs.readFileSync(jsonConfig).toString();
-        var jsonParsed = JSON.parse(jsonConfigFile);
-
-        linkedin_deeplink_url = jsonParsed.app_deeplink.url_scheme.replace(/\s/g, '')
-        linkedin_deeplink_host = jsonParsed.app_deeplink.url_host.replace(/\s/g, '')
-        linkedin_deeplink_path = jsonParsed.app_deeplink.url_path.replace(/\s/g, '')
+        linkedin_deeplink_url = jsonConfig.app_deeplink.url_scheme.replace(/\s/g, '')
+        linkedin_deeplink_host = jsonConfig.app_deeplink.url_host.replace(/\s/g, '')
+        linkedin_deeplink_path = jsonConfig.app_deeplink.url_path.replace(/\s/g, '')
     } catch {
-        throw new Error("Missing configuration file or error trying to obtain the configuration.");
+        throw new Error("Error trying to obtain the configuration.");
     }
-
+    
     //go inside the AndroidManifest and change values for schema, host and path
-    var manifestPath = path.join(projectRoot, 'platforms/android/app/src/main/AndroidManifest.xml');
-    var manifestFile = fs.readFileSync(manifestPath).toString();
-    var etreeManifest = et.parse(manifestFile);
-  
-    var dataTags = etreeManifest.findall('./application/activity/intent-filter/data[@android:scheme="OAUTH_DEEPLINK_SCHEME"]');
-    for (var i = 0; i < dataTags.length; i++) {
-        var data = dataTags[i];
-        data.set("android:scheme", linkedin_deeplink_url);
-    }
+    let manifestPath = path.join(projectRoot, 'platforms/android/app/src/main/AndroidManifest.xml');
+    let manifestFile = fs.readFileSync(manifestPath).toString();
+    let etreeManifest = et.parse(manifestFile);
+
+    let dataTags = etreeManifest.findall('./application/activity/intent-filter/data[@android:scheme="OAUTH_DEEPLINK_SCHEME"]');
+    dataTags.forEach((data) => data.set("android:scheme", linkedin_deeplink_url));
     dataTags = null;
 
     dataTags = etreeManifest.findall('./application/activity/intent-filter/data[@android:host="OAUTH_DEEPLINK_HOST"]');
-    for (var i = 0; i < dataTags.length; i++) {
-        var data = dataTags[i];
-        data.set("android:host", linkedin_deeplink_host);
-    }
-
+    dataTags.forEach((data) => data.set("android:host", linkedin_deeplink_host));
     dataTags = null;
+
     dataTags = etreeManifest.findall('./application/activity/intent-filter/data[@android:path="OAUTH_DEEPLINK_PATH"]');
-    for (var i = 0; i < dataTags.length; i++) {
-        var data = dataTags[i];
-        data.set("android:path", linkedin_deeplink_path);
-    }
+    dataTags.forEach((data) => data.set("android:path", linkedin_deeplink_path));
 
-    var resultXmlManifest = etreeManifest.write();
+    let resultXmlManifest = etreeManifest.write();
     fs.writeFileSync(manifestPath, resultXmlManifest);
-    
-};
+}
 
-function copyFacebookPreferences(projectRoot) {
+function copyFacebookPreferences(jsonConfig, projectRoot) {
 
-    var facebook_client_appId = "";
-    var facebook_client_token = "";
+    let facebook_client_appId = "";
+    let facebook_client_token = "";
 
-    var jsonConfig = "";
     try {
-        jsonConfig = path.join(projectRoot, 'www/json-config/SocialLoginsConfigurations.json');
-        var jsonConfigFile = fs.readFileSync(jsonConfig).toString();
-        var jsonParsed = JSON.parse(jsonConfigFile);
-    
-        jsonParsed.app_configurations.forEach(function(configItem) {
+        jsonConfig.app_configurations.forEach( (configItem) =>{
             if ((configItem.provider_id == ProvidersEnum.facebook) && 
                 (configItem.application_type_id == ApplicationTypeEnum.android)) {
                 facebook_client_appId = configItem.client_id;
@@ -81,21 +71,23 @@ function copyFacebookPreferences(projectRoot) {
         throw new Error("Missing configuration file or error trying to obtain the configuration.");
     }
 
-    var stringsPath = path.join(projectRoot, 'platforms/android/app/src/main/res/values/strings.xml');
-    var stringsFile = fs.readFileSync(stringsPath).toString();
-    var etreeStrings = et.parse(stringsFile);
+    let stringsPath = path.join(projectRoot, 'platforms/android/app/src/main/res/values/strings.xml');
+    let stringsFile = fs.readFileSync(stringsPath).toString();
+    let etreeStrings = et.parse(stringsFile);
 
-    var appIdTags = etreeStrings.findall('./string/[@name="facebook_app_id"]');
-    for (var tag of appIdTags) {
+    let appIdTags = etreeStrings.findall('./string/[@name="facebook_app_id"]');
+    for (let tag of appIdTags) {
         tag.text = facebook_client_appId;
     }
 
-    var clientTokenTags = etreeStrings.findall('./string/[@name="facebook_client_token"]');
-    for (var tag of clientTokenTags) {
+    let clientTokenTags = etreeStrings.findall('./string/[@name="facebook_client_token"]');
+    for (let tag of clientTokenTags) {
         tag.text = facebook_client_token;
     }
 
-    var resultXmlStrings = etreeStrings.write();
+    let resultXmlStrings = etreeStrings.write();
     fs.writeFileSync(stringsPath, resultXmlStrings);
 
 };
+
+
