a8b51378c854a68255de6e8d787f5438d0c168e2
Ricardo Silva <97543217+OS-ricardomoreirasilva@users.noreply.github.com>
2024-01-30 16:02:32 +0000

    fix: Deep Link to Specific Screen Issue (#48)
    Fix the issue with the deep link, where the plugin would not delegate the `ApplicationID` deep link to `CDVAppDelegate`, not making the expected route to the desired screen.
    
    References: https://outsystemsrd.atlassian.net/browse/RMET-3139

--- src/ios/AppDelegate+OSSocialLogins.m
+++ src/ios/AppDelegate+OSSocialLogins.m
@@ -20,10 +20,27 @@
 }
 
 - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
-    return [SocialLoginsApplicationDelegate.shared
-            application:app
-            openURL:url
-            options:options];
+    // gets the passed url's `urlScheme`
+    NSString *currentUrlScheme = [url.absoluteString componentsSeparatedByString:@"://"].firstObject;
+    
+    // check if it should delegate to the library or to `CDVAppDelegate`
+    return [self shouldDelegateToLibrary:currentUrlScheme] ? [SocialLoginsApplicationDelegate.shared application:app openURL:url options:options] : [super application:app openURL:url options:options];
+}
+
+- (BOOL)shouldDelegateToLibrary:(NSString *)openURLScheme {
+    // fetch all app's `utlTypes`
+    NSArray *urlTypes = [NSBundle.mainBundle objectForInfoDictionaryKey:@"CFBundleURLTypes"];
+    NSPredicate *predicate = [NSPredicate predicateWithBlock:^BOOL(id  evaluatedObject, NSDictionary<NSString *,id> * bindings) {
+        // fetch the `urlSchemes` associated to the evaluated `urlType`
+        NSArray * urlSchemeArray = evaluatedObject[@"CFBundleURLSchemes"];
+        // verifies if the `urlType` relates with the `urlScheme` of the to-be-opened URL
+        return [urlSchemeArray containsObject:openURLScheme];
+    }];
+    
+    // applying the filter, it fetches the `urlName` of the matching `urlType`
+    NSString *urlName = [urlTypes filteredArrayUsingPredicate:predicate].firstObject[@"CFBundleURLName"];
+    // checks if the matching `urlName` is one that can be delegated to the library.
+    return [@[@"Google", @"Facebook", @"DeepLinkScheme"] containsObject:urlName];
 }
 
 @end
