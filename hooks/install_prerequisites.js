6ba163f9720c5752c5817bd49cde73a401d53582
Marta Carlos <marta.carlos@outsystems.com>
2024-03-25 12:07:09 +0000

    feat: update hook error flow
    - updates error message with placeholder for plugin specific errors
    
    https://outsystemsrd.atlassian.net/browse/RMET-3310

--- hooks/install_prerequisites.js
+++ hooks/install_prerequisites.js
@@ -5,10 +5,10 @@ module.exports = function (context) {
   var child_process = require('child_process'),
       deferral = require('q').defer();
 
-  var output = child_process.exec('npm install', {cwd: __dirname}, function (error) {
+  child_process.exec('npm install', {cwd: __dirname}, function (error) {
     if (error !== null) {
       console.log('exec error: ' + error);
-      deferral.reject('npm installation failed');
+      throw new Error("OUTSYSTEMS_PLUGIN_ERROR: Couldn't finish npm install.");
     }
     else {
       deferral.resolve();
